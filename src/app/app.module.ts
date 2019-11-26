88a9ec8acb3547bb287b23f4d8731b89c2b1588d
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2019-11-26 17:55:59 +0200

    add components with messages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { CalendarModule } from '@progress/kendo-angular-dateinputs';
+
+import '@progress/kendo-angular-intl/locales/es/all';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    CalendarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
